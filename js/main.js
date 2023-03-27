68bcff615e3d97ac7ed0d897fda2479ccf59fec4
Thyago Lobato <thyagollobato@gmail.com>
2023-03-27 16:26:04 -0300

    Fim do projeto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
 
-const form = document.getElementById("novoItem") 
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []   
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];   
 
 // Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
 itens.forEach( (elemento) => {    
@@ -28,9 +28,9 @@ form.addEventListener("submit", (evento) => {
 
         atualizaElemento(itemAtual);
 
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         criaElemento(itemAtual);
 
@@ -56,9 +56,30 @@ function criaElemento(item) {
 
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
