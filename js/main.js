8ede25095ca82e38d3e95de846de146bf55d9ebb
Thyago Lobato <thyagollobato@gmail.com>
2023-03-27 14:31:52 -0300

    Armazenando dados na Web

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+    nome.value = "";
+    quantidade.value = "";
 })
 
 function criaElemento(nome, quantidade) {
@@ -19,4 +25,12 @@ function criaElemento(nome, quantidade) {
 
     lista.appendChild(novoItem);
 
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(itens));
 }
